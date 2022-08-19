7277fb2047ec4c68b08a60e4a8ba5f5f02a5bcec
TulioTofani <tuliotoff@gmail.com>
2022-08-19 19:56:04 -0300

    resolução dos exercícios 4.2

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,87 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let media;
+let result;
+let odd = [];
+let even = [];
+
+
+// let largest = numbers[0];
+
+// Exercício 2
+// for (let index = 0; index < numbers.length; index += 1){
+//     sum += numbers[index];
+// }
+// console.log(sum);
+
+// Exercício 3
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     sum += numbers[index];
+// }
+// console.log(sum / numbers.length);
+
+// Exercício 4
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     sum += numbers[index];
+// }
+// media = (sum / numbers.length);
+// if (media > 20){
+//     console.log('valor maior que 20');
+// } else {
+//     console.log('valor menor ou igual a 20');
+// }
+
+// Exercício 5
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     if (largest < numbers[index]){
+//         largest = numbers[index];
+//     }
+// }
+// console.log(largest);
+
+// Exercício 6
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// let media;
+// let result;
+// let largest = numbers[0];
+
+// Exercício 6
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     if ((numbers[index] % 2 !== 0)){
+//         odd.push(numbers[index]);
+//     } else {even.push(numbers[index])};
+// }
+// if (odd.length > 0){
+//     console.log('Consto no array', odd.length, 'números impares.');
+// } else {console.log('nenhum valor ímpar')};
+
+// Exercício 7
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     if (largest < numbers[index]){
+//         largest = numbers[index];
+//     }
+// }
+// console.log(largest);
+
+// Exercício 8
+
+// let arr = [];
+
+// for ( let index = 1; index <= 25; index += 1){
+//     arr.push(index);
+// }
+// console.log(arr);
+
+// // Exercício 9
+
+// let divisaoPor2 = [];
+// for (let index = 0; index < arr.length; index += 1){
+//     divisaoPor2.push(arr[index] / 2);
+// }
+// console.log(divisaoPor2);
