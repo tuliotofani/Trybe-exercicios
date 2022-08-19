f0aeac96da755b427abf4a15cd61fb2bd64f77d8
TulioTofani <tuliotoff@gmail.com>
2022-08-19 16:00:16 -0300

    estudos do segundo dia

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-lógica-de-programacao/estudo2.js
@@ -0,0 +1,43 @@
+// // Exercício 1
+// let menu = ["Home", "Serviços", "Portifólio", "LInks"];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// // Exercício 2
+// let menu = ["Home", "Serviços", "Portifólio", "LInks"];
+// let menuServices = menu.indexOf("Portifólio");
+
+// console.log(menuServices);
+
+// // Exercício 3
+// let menu = ["Home", "Serviços", "Portifólio", "LInks"];
+// menu.push("Contato");
+
+// console.log(menu);
+
+// Exercício 4
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// let result;
+
+// for (let index = 0; index < groceryList.length; index += 1){
+//     console.log(groceryList[index]);
+// }
+
+// Exercício 5
+// let names = ['joão', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let elements of names){
+//     console.log(elements);
+// }
+
+// let counter = 0;
+//  while(counter !== 5){
+//     counter += 1;
+//  }
+
+//  console.log(counter)
+
+const teste = ['a', 'b', 'c'];
+// teste[1] = 't';
+console.log(teste);
