23aabb3571120d456f9d3ec87c228892ce09cc80
TulioTofani <tuliotoff@gmail.com>
2022-08-22 14:09:26 -0300

    Resolução academia de lógica

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/academia-logica.js
@@ -0,0 +1,48 @@
+// Exercício 1
+
+// const arr = [1, 21, 23, 33, 34, 55, 58, 100, 102, 45];
+// let variavel = 1;
+// let result = [];
+
+//     if (arr.indexOf(variavel) >= 0){
+//         console.log(arr.indexOf(variavel));
+//     } else {console.log('Elemento não encontrado no array')}
+
+// Exercício 2
+
+// let arr = [];
+// let numerosImpares = [];
+
+// for (let index = 1; index <= 50; index += 1){
+//     arr.push(index);
+// }
+// for (let index = 0; index < arr.length; index += 1){
+//     if (arr[index] % 2 == 1){
+//         numerosImpares.push(arr[index]);
+//         console.log(numerosImpares);
+//     }
+// }
+
+// Exercício 3
+
+// let valorInicial = 13;
+// let valorFinal = 66;
+// let arr = [];
+// let numerosImpares = [];
+
+
+// for (let index = valorInicial; index <= valorFinal; index += 1){
+//     arr.push(index);
+// }
+// for (let index = 0; index < arr.length; index += 1){
+//     if (arr[index] % 3 == 0){
+//         numerosImpares.push(arr[index]);
+//         console.log(numerosImpares);
+//     }
+// }
+
+
+
+
+
+
