ebccf5b627749bafbdc38df35cf394ba734f4f29
TulioTofani <tuliotoff@gmail.com>
2022-08-22 18:47:23 -0300

    resolução exercícios da aula ao vivo

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-aula-ao-vico.js
@@ -0,0 +1,66 @@
+// Exercicio 1
+
+// let numeros = 0;
+
+// for (let index = 1; index <= 50; index += 1){
+//     numeros += index;
+// }
+// console.log(numeros);
+
+// let numeros = [];
+// let result = [];
+
+// for (let index = 2; index <= 150; index += 1){
+//     numeros.push(index);
+//     if (index % 3 === 0){
+//         result.push(index);
+//     }
+// }
+// if (result.length === 50){
+//     console.log('mensagem secreta');
+// } else {console.log('erro')}
+
+
+// Exercicio 3
+let player1 = ['pedra', 'papel', 'tesoura'];
+let player2 = ['pedra', 'papel', 'tesoura'];
+let player1Jogo = Math.floor(Math.random()*3)
+let player2Jogo = Math.floor(Math.random()*3)
+
+if (player1[player1Jogo] === player2[player2Jogo]){
+    console.log('A tie!!');
+} else if (player1[player1Jogo] === 'pedra' && player2[player2Jogo] === 'papel'){
+    console.log('Player 2 wins!!');
+} else if (player1[player1Jogo] === 'pedra' && player2[player2Jogo] === 'tesoura'){
+    console.log('Player 1 wins!!');
+} else if (player1[player1Jogo] === 'papel' && player2[player2Jogo] === 'pedra'){
+    console.log('Player 1 wins!!');
+} else if (player1[player1Jogo] === 'papel' && player2[player2Jogo] === 'tesoura'){
+    console.log('Player 2 wins!!');
+} else if (player1[player1Jogo] === 'tesoura' && player2[player2Jogo] === 'pedra'){
+    console.log('Player 2 wins!!');
+} else if (player1[player1Jogo] === 'tesoura' && player2[player2Jogo] === 'papel'){
+    console.log('Player 1 wins!!');
+}
+console.log('Player 1 jogou: ', player1[player1Jogo]);
+console.log('Player 2 jogou: ', player2[player2Jogo]);
+
+
+
+// Exercício 4
+// let age = 15;
+// const maioridade = age >= 18 ? 'Maior de idade' : 'Menor de idade';
+// console.log(maioridade);
+
+// Exercício 5
+// let carolzita = 27;
+// let murilo = 27;
+// let baete = 17;
+
+// if (carolzita < murilo && carolzita < baete){
+//     console.log('Carolzita é a pessoa mais nova');
+// } else if (murilo < baete) {
+//     console.log('Murilo é a pessoa mais nova');
+// } else if (baete < murilo){
+//     console.log('Baête é a pesso a mais nova');
+// } else {console.log('Temos idades iguais')}
