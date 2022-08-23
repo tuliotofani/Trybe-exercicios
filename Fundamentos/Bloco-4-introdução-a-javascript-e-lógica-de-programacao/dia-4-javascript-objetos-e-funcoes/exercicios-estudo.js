1cf87ae5edc800946ca1bb2f9b01444f2fea7a1a
TulioTofani <tuliotoff@gmail.com>
2022-08-23 18:36:05 -0300

    adiciona resolução dos exercícios de estudo

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios-estudo.js
@@ -0,0 +1,55 @@
+// Exercícios Objetos
+// let player = {
+//     name: 'Marta',
+//     lastNae: 'Silva',
+//     age: 34,
+//     medals: {
+//         golden: 2,
+//         silver: 3,
+//     }
+// }
+
+// console.log('A Jogadora ' + player.name + ' ' + player.lastNae + ' tem ' + player.age + ' anos de idade.');
+
+// player.bestOfTheWolrd = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// // console.log(player);
+
+// console.log('A Jogadora ' + player.name + ' ' + player.lastNae + ' foi eleita a molhor do mundo ' + player.bestOfTheWolrd.length + ' vezes.');
+
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.')
+
+// Exercícios for/in for/of
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+// };
+
+// for (let key in names){
+//     console.log('Olá ' + names[key]);
+// }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020,
+// };
+
+// for (let key in car){
+//     console.log(car[key]);
+// };
+
+// function sum(a, b) {
+//     return a + b;
+
+//   }
+//   console.log(sum(1, 2));
+
+
+// function greetingsMessage(name) {
+//     return ("Boas vindas, " + name);
+//   }
+  
+//   console.log(greetingsMessage('Túlio'));
+  
