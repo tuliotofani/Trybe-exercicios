50621f7aba9d57bff5ba9325ab6b57951c6ee593
TulioTofani <tuliotoff@gmail.com>
2022-08-23 20:10:23 -0300

    resolução exercícios estudo sobre funções

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-lógica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicios-estudo-funcoes.js
@@ -0,0 +1,69 @@
+// Exercício 1
+
+// function sum(a, b){
+//     return a + b;
+// }
+
+// function sub(a,b){
+//     return a - b;
+// }
+
+// function mult(a,b){
+//     return a * b;
+// }
+
+// function div(a,b){
+//     return a / b;
+// }
+
+// function mod(a,b){
+//     return a % b;
+// }
+
+// Exercício 2
+
+// function biggestNumber(a,b){
+//     if (a > b){
+//         return a + ' é maior que ' + b;
+//     } 
+//         return b + ' é maior que ' + a;
+// }
+// console.log(biggestNumber(25,28));
+
+// Exercício 3
+
+// function biggestNumber(a,b,c){
+//     if (a > b && a > c){
+//         return a + ' é o maior número';
+//     } else if (b > c){
+//         return b + ' é o maior número';
+//     } else if (c > b && c !== a){
+//         return c + ' é o maior númeor';
+//     } 
+//     return 'Temos númeors maiores repetidos';
+// }
+// console.log(biggestNumber(17,7,17));
+
+// Exercício 4
+
+// function number(a){
+//     if (a > 0){
+//         return a + ' é positivo.';
+//     } else if (a < 0){
+//         return a + ' é negativo.';
+//     }
+//         return 'zero';
+// }
+// console.log(number(0));
+
+// Exercício 5
+
+// function isTriangle(a,b,c){
+//     if (a + b + c === 180 && a > 0 && b > 0 && c > 0){
+//         return true;
+//     } else if (a <= 0 || b <= 0 || c <= 0){
+//         return 'erro ângulo inválido';
+//     } 
+//         return false;
+// }
+// console.log(isTriangle(10,60,70));
