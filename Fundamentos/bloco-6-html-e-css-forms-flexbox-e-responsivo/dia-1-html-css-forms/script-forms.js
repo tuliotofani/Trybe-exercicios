11735825cc0371bfbd5ddf12bae409c8392bb687
TulioTofani <tuliotoff@gmail.com>
2022-09-06 22:07:09 -0300

    adicona checkvalidity

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script-forms.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script-forms.js
@@ -11,21 +11,9 @@ const userEmail = document.querySelector('#email');
 const userText = document.querySelector('#seu-texto');
 
 function checkValidity() {
-    if (userName.checkValidity() === true && userEmail.checkValidity() === true && userText === true) {
+    if (userName.checkValidity() === true && userEmail.checkValidity() === true && userText.checkValidity() === true) {
         alert('Dados enviados com sucesso!')
     } else {
         alert('Error!')
     }
 }
-
-// submit.addEventListener('click', checkValidity);
-
-// submit.addEventListener('click', () => {
-//     if (userName.checkValidity()) {
-//         alert('Dados enviados com sucesso!')
-//         // event.preventDefault();
-//     } else {
-//         alert('Error!')
-//         // event.preventDefault();
-//     }
-// });
